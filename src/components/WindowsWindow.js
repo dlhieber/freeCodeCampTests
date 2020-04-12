1af201da8a62d3522dbabf71ca8148d8c60a17e2
dlhieber <dan_hiebert@ymail.com>
2020-04-12 16:06:05 -0600

    Fixed query parameter

--- src/components/WindowsWindow.js
+++ src/components/WindowsWindow.js
@@ -2,27 +2,17 @@ import React, { Component } from 'react';
 import { Window, TitleBar, Text } from 'react-desktop/windows';
 import Navi from './Navi';
 
+
 export default class WindowsWindow extends Component {
   static defaultProps = {
     color: '#cc7f29',
     theme: 'light'
   };
-  constructor(props){
-    super(props);
-    this.state={test:''}
-  }
-  componentDidMount(){
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    
-    const openTest=urlParams.get('test')||'Random Generator';
 
-
-    this.setState({test:openTest});
-    this.forceUpdate();
-  }
   render() {
-    console.log(this.state.test);
+
+    
+
     return (
       <div id="quote-box">
       <Window
@@ -35,8 +25,7 @@ export default class WindowsWindow extends Component {
         
       >
         <TitleBar title="Having Fun with Windows Style React Components" controls/>
-        
-        <Navi test={this.state.test} theme={this.props.theme} color={this.props.theme === 'dark' ? 'white' : '#333'}/>
+        <Navi test={this.props.search} theme={this.props.theme} color={this.props.theme === 'dark' ? 'white' : '#333'}/>
 
       </Window></div>
     );
